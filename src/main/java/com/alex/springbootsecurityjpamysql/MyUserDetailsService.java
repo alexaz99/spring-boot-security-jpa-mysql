733253d785f73f06461a2df7f1ad49c82d794698
Alex <azakotyansky@gmail.com>
2021-04-10 22:29:51 -0400

    Initial revision

--- src/main/java/com/alex/springbootsecurityjpamysql/MyUserDetailsService.java
+++ src/main/java/com/alex/springbootsecurityjpamysql/MyUserDetailsService.java
@@ -1,10 +1,14 @@
 package com.alex.springbootsecurityjpamysql;
 
 import com.alex.springbootsecurityjpamysql.models.MyUserDetails;
+import com.alex.springbootsecurityjpamysql.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Create my implementation of UserDetailsService and implement
@@ -15,10 +19,18 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
 
         // first create a hard coded user by implemention UserDetails
-        return new MyUserDetails(s);
+        //return new MyUserDetails(s);
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not Found: " + userName));
+
+        return new MyUserDetails(user.get());
     }
 }
--- src/main/java/com/alex/springbootsecurityjpamysql/SpringBootSecurityJpaMysqlApplication.java
+++ src/main/java/com/alex/springbootsecurityjpamysql/SpringBootSecurityJpaMysqlApplication.java
@@ -2,8 +2,10 @@ package com.alex.springbootsecurityjpamysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SpringBootSecurityJpaMysqlApplication {
 
 	public static void main(String[] args) {
